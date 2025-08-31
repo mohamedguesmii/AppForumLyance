e15ceea5f8218440f6a6550b39c7147e0c3ddc30
Mohamed Guesmi <75278320+mohamedguesmii@users.noreply.github.com>
2025-08-31 07:31:24 +0100

    Update IActualite.java

--- EvenementCrud/src/main/java/tn/esprit/devoir/repository/IActualite.java
+++ EvenementCrud/src/main/java/tn/esprit/devoir/repository/IActualite.java
@@ -1,12 +1,17 @@
 package tn.esprit.devoir.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import tn.esprit.devoir.entite.Actualite;
 
 @Repository
+public interface IActualite extends JpaRepository<Actualite, Long> {
 
-public interface IActualite extends JpaRepository<Actualite,Long> {
+    // Si reacts est un boolean
+    // Actualite findTopByReactsIsTrue();
 
-   // Actualite findTopByReactsIsTrue();
+    // Si reacts est une collection (ex: List reacts)
+    @Query("SELECT a FROM Actualite a WHERE SIZE(a.reacts) > 0 ORDER BY a.id DESC")
+    Actualite findTopWithReacts();
 }
