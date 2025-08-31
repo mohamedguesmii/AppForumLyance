036d1d07c94ddf0cf329b95dde7d2bfb9d2445ab
Mohamed Guesmi <75278320+mohamedguesmii@users.noreply.github.com>
2025-08-31 06:58:51 +0100

    Update IActualite.java

--- EvenementCrud/src/main/java/tn/esprit/devoir/repository/IActualite.java
+++ EvenementCrud/src/main/java/tn/esprit/devoir/repository/IActualite.java
@@ -1,20 +1,12 @@
 package tn.esprit.devoir.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import tn.esprit.devoir.entite.Actualite;
 
-import java.util.List;
-
 @Repository
-public interface IActualite extends JpaRepository<Actualite, Long> {
 
-    // Méthode pour récupérer la dernière actualité avec reacts = true
-    @Query("SELECT a FROM Actualite a WHERE a.reacts = true ORDER BY a.id DESC")
-    Actualite findTopActiveActualite();
+public interface IActualite extends JpaRepository<Actualite,Long> {
 
-    // Exemple : récupérer toutes les actualités actives
-    @Query("SELECT a FROM Actualite a WHERE a.reacts = true ORDER BY a.id DESC")
-    List<Actualite> findAllActiveActualites();
+    Actualite findTopByReactsIsTrue();
 }
